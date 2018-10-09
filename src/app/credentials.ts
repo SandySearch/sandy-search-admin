44015b33f1fed3c45007dfa3185f2e1512fd7f2f
mkobar <mike@exlorev.com>
2018-10-09 13:47:48 -0400

    point to sandy-demo data

---
+++ src/app/credentials.ts
@@ -0,0 +1,14 @@
+// sandy-demo
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBX_snWMnbHI81o4yHlTyY_BcUw1T32FSU",
+  authDomain: "sandy-demo-39fbe.firebaseapp.com",
+  databaseURL: "https://sandy-demo-39fbe.firebaseio.com",
+  projectId: "sandy-demo-39fbe",
+  storageBucket: "sandy-demo-39fbe.appspot.com",
+  messagingSenderId: "146051658839"
+}
+
+export const googleMapsKey = 'AIzaSyD8HNt2Du1RFCgbT3eUXUU0jzflA-qK0Ak'
+
+//export const appVersion = '0.0.7'
