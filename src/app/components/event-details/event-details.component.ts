a9f3280e734459e234c381d10d8a657e6023f102
mkobar <mike@exlorev.com>
2018-12-12 13:26:17 -0500

    added point maps to events - temp

--- src/app/components/event-details/event-details.component.ts
+++ src/app/components/event-details/event-details.component.ts
@@ -6,12 +6,23 @@ import * as firebase from 'firebase';
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
-  styleUrls: ['./event-details.component.css']
+  // as per https://github.com/SebastianM/angular-google-maps/issues/1018
+  styles: [`
+    .sebm-google-map-container {
+       height: 200px;
+     }
+     .agm-map {
+       height: 200px;
+     }
+  `],
+  //styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent implements OnInit {
 
 	id:any;
 	event_details:any;
+        lat: number // eslint-disable-line no-undef
+        lng: number // eslint-disable-line no-undef
 
   constructor(
   private firebaseService : FirebaseService,
@@ -24,8 +35,9 @@ export class EventDetailsComponent implements OnInit {
 	  console.log(this.id);
 	  
 	  this.firebaseService.getEventDetails(this.id).subscribe(event_details => {
-      this.event_details = event_details;
-
+	  this.event_details = event_details;
+	  this.lat = this.event_details.nePointLat;
+	  this.lng = this.event_details.nePointLon;
     });
 	  
   }
