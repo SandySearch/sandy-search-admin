39de0b006ded5d6e3a32add7798783241b4b6b80
mkobar <mike@exlorev.com>
2018-12-12 13:25:48 -0500

    added maps to listing

--- src/app/components/listing/listing.component.ts
+++ src/app/components/listing/listing.component.ts
@@ -3,15 +3,28 @@ import {FirebaseService} from '../../services/firebase.service';
 import {Router,ActivatedRoute,Params} from '@angular/router';
 import * as firebase from 'firebase';
 
+
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
-  styleUrls: ['./listing.component.css']
+  // as per https://github.com/SebastianM/angular-google-maps/issues/1018
+  styles: [`
+    .sebm-google-map-container {
+       height: 200px;
+     }
+     .agm-map {
+       height: 200px;
+     }
+  `],
+  //styleUrls: ['./listing.component.css']
 })
 export class ListingComponent implements OnInit {
 	id:any;
 	listing:any;
 	//imageUrl:any;
+        lat: number // eslint-disable-line no-undef
+        lng: number // eslint-disable-line no-undef
+        zoom: number = 10 // eslint-disable-line no-undef
 	
   constructor(
 	private firebaseService : FirebaseService,
@@ -24,15 +37,16 @@ export class ListingComponent implements OnInit {
 	  console.log(this.id);
 	  
 	  this.firebaseService.getListingDetails(this.id).subscribe(listing => {
-             this.listing = listing;
+	     this.listing = listing;
+	     this.lat = this.listing.lat;
+	     this.lng = this.listing.lon;
+	     this.zoom = 10;
     });
 	  
   }
   
-  onDeleteClick(){
-	  
-	  this.firebaseService.deleteListing(this.id);
-	  
+  onDeleteClick(){ 
+	  this.firebaseService.deleteListing(this.id);	  
 	  this.router.navigate(['/listings']);
   }
 
